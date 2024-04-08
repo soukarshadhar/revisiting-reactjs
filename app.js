0a38d5e93dc8dca7299a781d7a14532fbebbdc94
Soukarsha Dhar <soukarshadhar91@gmail.com>
2024-04-08 17:12:42 +0530

    Updated classname

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const root = document.getElementById('container');
-const list = React.createElement('ul', { id: 'list'}, [React.createElement('li', {}, 'First item'), React.createElement('li', {}, 'Second item')]);
+const root = document.getElementById('root');
+const list = React.createElement('ul', { id: 'list'}, 
+  [
+    React.createElement('li', {}, 'First item'),
+    React.createElement('li', {}, 'Second item')
+  ]
+);
 const reactRoot = ReactDOM.createRoot(root);
 reactRoot.render(list);
