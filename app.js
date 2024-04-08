ba50f44fa46dd4712fea9f1fa19c9ea1f5a743d0
Soukarsha Dhar <soukarshadhar91@gmail.com>
2024-04-08 17:57:37 +0530

    Added two more list items

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const root = document.getElementById('root');
-const list = React.createElement('ul', { id: 'list'}, 
+const twoList = React.createElement(React.Fragment, {},
   [
-    React.createElement('li', {}, 'First item'),
-    React.createElement('li', {}, 'Second item')
-  ]
-);
+    React.createElement('ul', { id: 'list1'}, 
+      [
+        React.createElement('li', {}, 'First item'),
+        React.createElement('li', {}, 'Second item')
+      ]
+    ),
+    React.createElement('ul', { id: 'list2'}, 
+      [
+        React.createElement('li', {}, 'Third item'),
+        React.createElement('li', {}, 'Fourth item')
+      ]
+    )
+  ]);
+
 const reactRoot = ReactDOM.createRoot(root);
-reactRoot.render(list);
+reactRoot.render(twoList);
